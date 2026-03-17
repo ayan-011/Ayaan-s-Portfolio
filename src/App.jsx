79491d1d836c82a2b77d74ac12cfe8ea7d326a1f
ayan-011 <ayansaifi2nd@gmail.com>
2026-03-17 14:21:14 +0530

    enhancement

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ import { Navbar } from './Navbar';
 import { Status } from './Status'; 
 import { Project } from './projects/Projects';
 import Contact from './Contact';
- 
+import { IoIosArrowForward } from 'react-icons/io';
+import { Link } from 'react-router-dom';
+  
 
 function App() {
   return (
@@ -18,7 +20,7 @@ function App() {
         {/* NAVBAR  */}
 
 
-    <div className="blackEmpty lg:py-11 py-7   md:py-9 w-full bg-red-    relative z-10"></div>
+    <div className="blackEmpty lg:py-11 py-7 md:py-9 w-full relative z-10 "></div>
       
       <div className=" w-full max-w-6xl grid sm:gap-3 gap-1 bg-red-90 p-3 
                        grid-cols-2 auto-rows-[120px]   /* Mobile */
@@ -76,14 +78,70 @@ function App() {
 
 
         {/* Box 4 */}
-        <div className=" col-span-2 row-span-2 sm:order-6 sm:col-span-2 lg:order-4 border border-[#525252] rounded-lg lg:rounded-xl bg-[#1e1e1e] overflow-y-scroll scrollbar-hide px-2 " > 
-           <Skills/>
+        <div className=" col-span-2 row-span-2 sm:order-6 sm:col-span-2 lg:order-4 border border-[#525252] rounded-lg lg:rounded-xl bg-[#1e1e1e] sm:overflow-y-scroll scrollbar-hide px-2 md:bg-none
+        // mobile
+        overflow-hidden
+          relative
+        " > 
+        <div className="absolute bottom-0 left-0 w-full h-28
+  bg-gradient-to-t from-[#131313] via-[#161616a8] to-transparent
+  sm:hidden pointer-events-none">
+</div>
+
+{/* Mobile Button */}
+<div className="absolute bottom-4 left-0 w-full flex justify-center sm:hidden z-20  ">
+  <Link to="/viewallskills">
+  <button className="px-3 py-1.5 text-xs border border-zinc-400 
+    text-zinc-200 rounded-full flex items-center gap-1.5
+    backdrop-blur-sm bg-black/40
+       ">
+    
+    <span className="tracking-tight ">
+      View More
+    </span>
+
+    <IoIosArrowForward className="text-sm" />
+    
+  </button>
+  
+  </Link>
+</div>
+
+        
+           <  Skills/>
           </div>
 
 
 
         {/* Box 5 */}
-        <div className="col-span-2 row-span-4 sm:col-span-4 md:col-span-4 lg:col-span-4 row-span-2 lg:row-span-3 sm:order-4 border border-[#525252] rounded-lg lg:rounded-xl bg-[#1e1e1e] overflow-y-scroll scrollbar-hide px-2 flex flex-col" >
+        <div className="col-span-2 row-span-4 sm:col-span-4 md:col-span-4 lg:col-span-4 row-span-2 lg:row-span-3 sm:order-4 border border-[#525252] rounded-lg lg:rounded-xl bg-[#1e1e1e] sm:overflow-y-scroll scrollbar-hide px-2 flex flex-col 
+        // mobile
+          relative 
+          overflow-hidden
+        " >
+
+           <div className="absolute bottom-0 left-0 w-full h-52
+  bg-gradient-to-t from-[#000000ee] via-[#202020cc] to-transparent
+  sm:hidden pointer-events-none">
+</div>
+
+{/* Mobile Button */}
+<div className="absolute bottom-4 left-0 w-full flex justify-center sm:hidden z-20  ">
+  <button className="px-3 py-1.5 text-xs border border-zinc-400 
+    text-zinc-200 rounded-full flex items-center gap-1.5
+    backdrop-blur-sm bg-black/40
+       ">
+    
+    <Link to="/allprojects">
+  <button className="tracking-tight">
+    View More
+  </button>
+</Link>
+
+    <IoIosArrowForward className="text-sm" />
+    
+  </button>
+</div>
          
          <Project/>         
 
