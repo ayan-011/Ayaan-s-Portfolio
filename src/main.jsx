cc256dbaa957485daa3f16ec23dc23627c3f9ede
ayan-011 <ayansaifi2nd@gmail.com>
2026-03-17 14:21:51 +0530

    enhance contact section

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Allprojects from './projects/Allprojects.jsx'
+import ScrollToTop from './ScrollToTop/ScrollToTop.jsx'
+import Viewallskills from './Viewallskills.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+   <BrowserRouter>
+     <ScrollToTop />
+        <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/allprojects" element={<Allprojects />} />
+      <Route path="/viewallskills" element={<Viewallskills />} />
+      {/* <Route path="*" element={<NotFound />} /> */}
+    </Routes>
+    </BrowserRouter>
+    
   </StrictMode>,
 )
