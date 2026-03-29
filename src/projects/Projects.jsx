b01a898ae2e3ac5960662fbb7afaf8e0b10dbfd4
ayan-011 <ayansaifi2nd@gmail.com>
2026-03-29 12:18:06 +0530

    projects

--- src/projects/Projects.jsx
+++ src/projects/Projects.jsx
@@ -7,6 +7,14 @@ export function Project(){
      const cardsData = [
     {
       id: 1,
+      title: "Script Generator",
+      description: "Script Generator application that helps users quickly create structured and engaging scripts for various purposes. The project focuses on simplicity and efficiency, allowing users to generate content with minimal input while maintaining quality output.",
+      image: "/thumbnails/scriptgenerator.png",
+      live: "https://v0-you-tube-shorts-script-rosy.vercel.app/",
+      github: "https://github.com/ayan-011/script_generator"
+    },
+    {
+      id: 2,
       title: "Ecommerce Project",
       description: "A responsive e-commerce web application with product listings, cart management, and a streamlined checkout flow. Built with a focus on performance, clean UI, and intuitive user experience.",
       image: "/thumbnails/ecommerce.png",
@@ -14,7 +22,7 @@ export function Project(){
       github: "https://github.com/ayan-011/online-store"
     },
     {
-      id: 2,
+      id: 3,
       title: "Sportech",
       description: "Sportech is a sleek showcase website for a modern garage, highlighting its work, services, and features. Get a glimpse of the team's expertise, past projects, and what makes Sportech the go-to garage for car enthusiasts.",
       image: "/thumbnails/sportech.png",
